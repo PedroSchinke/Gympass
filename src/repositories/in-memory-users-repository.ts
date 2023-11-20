a49605218b5d1d9fb8218fb7cd4cb2ff9f85a33b
Pedro Schinke <pedro.schinke@outlook.com>
2023-11-20 17:00:15 -0300

    deleted in-memory-users-repository

--- src/repositories/in-memory-users-repository.ts
+++
@@ -1,9 +0,0 @@
-import { Prisma } from '@prisma/client'
-
-export class inMemoryUsersRepository {
-  public users: any[] = []
-
-  async create(data: Prisma.UserCreateInput) {
-    this.users.push(data)
-  }
-}
