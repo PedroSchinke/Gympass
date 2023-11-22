363963138fdf65f1caaa61fb5081ac2a66e198f9
Pedro Schinke <pedro.schinke@outlook.com>
2023-11-21 23:04:03 -0300

    created check-ins and gyms in-memory repositories

---
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -0,0 +1,47 @@
+import { CheckIn, Prisma } from '@prisma/client'
+import { CheckInsRepository } from '../check-ins-repository'
+import { randomUUID } from 'node:crypto'
+import dayjs from 'dayjs'
+
+export class InMemoryCheckInsRepository implements CheckInsRepository {
+  public items: CheckIn[] = []
+
+  async findByUserIdOnDate(userId: string, date: Date) {
+    const checkInOnSameDay = this.items.find((checkIn) => {
+      const checkInDate = dayjs(checkIn.created_at)
+      const isOnSameDate = dayjs(date).isSame(checkInDate, 'day')
+
+      return checkIn.user_id === userId && isOnSameDate
+    })
+
+    if (!checkInOnSameDay) {
+      return null
+    }
+
+    return checkInOnSameDay
+  }
+
+  async findManyByUserId(userId: string, page: number) {
+    return this.items
+      .filter((item) => item.user_id === userId)
+      .slice((page - 1) * 20, page * 20)
+  }
+
+  async countByUserId(userId: string) {
+    return this.items.filter((item) => item.user_id === userId).length
+  }
+
+  async create(data: Prisma.CheckInUncheckedCreateInput) {
+    const checkIn = {
+      id: randomUUID(),
+      user_id: data.user_id,
+      gym_id: data.gym_id,
+      validated_at: data.validated_at ? new Date(data.validated_at) : null,
+      created_at: new Date(),
+    }
+
+    this.items.push(checkIn)
+
+    return checkIn
+  }
+}
