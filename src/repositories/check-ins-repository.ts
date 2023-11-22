143f01d032289ed4f29f0e9455d488c5f9860b5b
Pedro Schinke <pedro.schinke@outlook.com>
2023-11-21 23:06:49 -0300

    created check-ins and gyms repositories

---
+++ src/repositories/check-ins-repository.ts
@@ -0,0 +1,8 @@
+import { CheckIn, Prisma } from '@prisma/client'
+
+export interface CheckInsRepository {
+  findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
+  findManyByUserId(userId: string, page: number): Promise<CheckIn[]>
+  countByUserId(userId: string): Promise<number>
+  create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
+}
